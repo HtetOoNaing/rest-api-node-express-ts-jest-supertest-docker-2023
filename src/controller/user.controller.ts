0f59205259653f1c3316c6871b396503cded5b11
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2023-09-02 23:25:51 +0630

    update create user middleware and controller

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -6,7 +6,7 @@ import { CreateUserInput } from "../schema/user.schema"
 export async function createUserHandler(req: Request<{}, {}, CreateUserInput["body"]>, res: Response) {
     try {
         const user = await createUser(req.body)
-        return user
+        return res.send(user)
     } catch (error:any) {
         logger.error(error)
         return res.status(409).send(error.message)
